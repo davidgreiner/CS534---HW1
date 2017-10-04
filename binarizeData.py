3e14857a1be34d9d4d563fb56d1420af8e8193df
David Greiner <mail@david-greiner.me>
2017-10-04 14:17:45 -0700

    Extract unique column data

--- binarizeData.py
+++ binarizeData.py
@@ -3,3 +3,14 @@ import numpy as np
 data = np.genfromtxt("income-data/income.train.txt",
            dtype=None,
            delimiter=", ")
+
+age = set(data['f0'])
+work = set(data['f1']))
+education = set(data['f2']))
+relationship = set(data['f3']))
+occupation = set(data['f4']))
+race = set(data['f5']))
+gender = set(data['f6']))
+workhours = set(data['f7']))
+country = set(data['f8']))
+salary = set(data['f9']))
