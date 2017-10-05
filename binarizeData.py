54f2f094bbb5b1c3f31021333a32a378aafd88d2
ferrpete <32526300+ferrpete@users.noreply.github.com>
2017-10-04 20:43:16 -0700

    First Commit
    Created feature vector that contains each unique data dimension (total # of dimensions is 258).  I also noticed that np.genfromtxt only generates a one-dimensional array where each line in the txt file is treated as one giant element.  I experimented a lot with this and found that np.column_stack is able to break this one-dimensional array into a two-dimensional array where each characteristic (age, education level, etc.) is indexed in a unique row and column.  If there is a better way to do this, feel to modify/change completely.  -Peter

--- binarizeData.py
+++ binarizeData.py
@@ -1,16 +1,34 @@
 import numpy as np
 
 data = np.genfromtxt("income-data/income.train.txt",
-           dtype=None,
+           dtype=[('f0', '<i4'), ('f1', 'U17'),
+                  ('f2', 'U13'), ('f3', 'U22'),
+                  ('f4', 'U18'), ('f5', 'U19'),
+                  ('f6', 'U7'), ('f7', '<i4'),
+                  ('f8', 'U27'), ('f9', 'U6')],
            delimiter=", ")
 
-age = set(data['f0'])
-work = set(data['f1']))
-education = set(data['f2']))
-relationship = set(data['f3']))
-occupation = set(data['f4']))
-race = set(data['f5']))
-gender = set(data['f6']))
-workhours = set(data['f7']))
-country = set(data['f8']))
-salary = set(data['f9']))
+age = np.unique(data['f0'])
+work = np.unique(data['f1'])
+education = np.unique(data['f2'])
+maritalstatus = np.unique(data['f3'])
+occupation = np.unique(data['f4'])
+race = np.unique(data['f5'])
+gender = np.unique(data['f6'])
+workhours = np.unique(data['f7'])
+country = np.unique(data['f8'])
+salary = np.unique(data['f9'])
+
+featureVector = np.hstack((age, work, education,
+                maritalstatus, occupation, race,
+                gender, workhours, country))
+
+## The genfromtxt function only produces a 1-dimensional
+## array of data.  np.column_stack breaks 'data' into
+## 2-dimensional array.
+
+arrayData = np.column_stack((data['f0'],data['f1'],
+                             data['f2'],data['f3'],
+                             data['f4'],data['f5'],
+                             data['f6'],data['f7'],
+                             data['f8'],data['f9']))
