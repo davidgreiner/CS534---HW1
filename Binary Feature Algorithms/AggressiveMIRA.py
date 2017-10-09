Fixed all files to work with binary and numerical processing.
Fixed all perceptron files to work with binary and numerical processing.

Binary Feature Algorithms/AggressiveMIRA.py
@@ -7,10 +7,9 @@ from Dev_Evaluator import DevEvaluator
 ## Basic Perceptron algorithm for binary classification
 ## of individuals earning less than or more than 50K/year.
 
-featureArray, trainDataArray = BinarizeData("train", shuffle=1)
-devDataArray = BinarizeData("dev")
+trainDataArray, devDataArray, featureArray = BinarizeData(shuffle=0)
 
-p = 0.5
+p = 0.9
 
 weightVector = np.zeros((len(featureArray)))
 epochCount = 0
@@ -46,40 +45,25 @@ while epochCount < totalEpoch:
             print("The error rate for epoch " + str(epochFraction) + \
                   " is " + str(devError) + "%")
 
-        if trainDataArray[i, -1] == '>50K':
+        xi = trainDataArray[i, :-1]
+
+        if trainDataArray[i, -1] == 1:
             y = 1
 
         else:
             y = -1
 
-        idx = np.isin(featureArray, trainDataArray[i, 0:-1])
+        decision = y*(np.dot(weightVector, xi))
 
-        decision = y*(weightVector[idx].sum() + weightVector[-1])
-
-        if decision < p:
-
-            if decision > 0:
-
-                marginCorrection = ( (p*y - np.sum(weightVector[idx]) - weightVector[-1]) / \
-                  np.sum(np.power(np.ones(len(trainDataArray[i, :])), 2)) )
-
-                weightVector[idx] = weightVector[idx] + \
-                  marginCorrection * np.ones(len(trainDataArray[i, 0:-1]))
-
-                weightVector[-1] = weightVector[-1] + marginCorrection
-
-##                check = y * (weightVector[idx].sum() + weightVector[-1])
-##                print(check)
+        if decision <= p:
             
-            marginCorrection = ( (y - np.sum(weightVector[idx]) - weightVector[-1]) / \
-              np.sum(np.power(np.ones(len(trainDataArray[i, :])), 2)) )
+            marginCorrection = ( (y - np.dot(weightVector, xi) ) / \
+              np.dot(xi, xi) )
 
-            weightVector[idx] = weightVector[idx] + \
-              marginCorrection * np.ones(len(trainDataArray[i, 0:-1]))
+            weightVector = weightVector + \
+              marginCorrection * xi
 
-            weightVector[-1] = weightVector[-1] + marginCorrection
-
-##            check = y * (weightVector[idx].sum() + weightVector[-1])
+##            check = y * (np.dot(weightVector, xi))
 ##            print(check)
             
         currentTrainingCount += 1

Binary Feature Algorithms/Dev_Evaluator.py
@@ -10,15 +10,15 @@ def DevEvaluator(weightVector, featureArray, devDataArray):
 
     for i in range(0, numberDevData):
 
-        if devDataArray[i, -1] == '>50K':
+        if devDataArray[i, -1] == 1:
             y = 1
 
         else:
             y = -1
 
-        idx = np.isin(featureArray, devDataArray[i, 0:-1])
+        xi = devDataArray[i, :-1]
 
-        if y*(weightVector[idx].sum() + weightVector[-1]) <= 0:
+        if y*(np.dot(weightVector, xi)) <= 0:
             
             devWrong += 1
 

Binary Feature Algorithms/avgNaivePerceptron.py
@@ -3,13 +3,12 @@ import time
 from binarizeData import BinarizeData
 from Dev_Evaluator import DevEvaluator
 
-featureArray, trainDataArray = BinarizeData("train")
-devDataArray = BinarizeData("dev")
+trainDataArray, devDataArray, featureArray = BinarizeData()
 
 weightVector = np.zeros(len(featureArray))
 cachedweight = np.zeros(len(featureArray))
 epochCount = 0
-totalEpoch = 1
+totalEpoch = 5
 numberTrainingData = len(trainDataArray)
 currentTrainingCount = 1
 bestErrorRate = 100.0
@@ -23,7 +22,8 @@ while epochCount < totalEpoch:
 
         if currentTrainingCount % 1000 == 0:
 
-            devError = DevEvaluator(cachedweight / (currentTrainingCount), featureArray, devDataArray)
+            devError = DevEvaluator(cachedweight / (currentTrainingCount),
+                                    featureArray, devDataArray)
 
             epochFraction = (i / numberTrainingData) + epochCount
 
@@ -35,28 +35,26 @@ while epochCount < totalEpoch:
                    " is " + str(devError) + "%")
 
 
-        if trainDataArray[i, -1] == '>50K':
+        if trainDataArray[i, -1] == 1:
             y = 1
 
         else:
             y = -1
 
-        idx = np.isin(featureArray, trainDataArray[i, 0:-1])
+        xi = trainDataArray[i, :-1]
 
-        if y * weightVector[idx].sum() + weightVector[-1] <= 0:
+        if y * np.dot(weightVector, xi) <= 0:
 
-            weightVector[idx] = weightVector[idx] + \
-                    y * np.ones((len(trainDataArray[i, 0:-1])))
+            weightVector = weightVector + \
+                    y * xi
 
-            weightVector[-1] = weightVector[-1] + y
-
-            cachedweight[idx] = cachedweight[idx] + y * currentTrainingCount * np.ones((len(trainDataArray[i, 0:-1])))
-
-            cachedweight[-1] = cachedweight[-1] + y * currentTrainingCount
+        cachedweight = cachedweight + weightVector
 
         currentTrainingCount += 1
 
     epochCount += 1
+
+finalWeight = cachedweight / currentTrainingCount
 
 print("The program ran for %s seconds" % (time.time() - startTime))
 print("The best error rate was " + str(bestErrorRate) + " at epoch " + \
@@ -64,7 +62,7 @@ print("The best error rate was " + str(bestErrorRate) + " at epoch " + \
 
 positiveFeatures = cachedweight.argsort()[-5:][::-1]
 print("The most positive features are: " + str(featureArray[positiveFeatures]) + \
-      " with weights of: " + str(cachedweight[positiveFeatures]))
+      " with weights of: " + str(finalWeight[positiveFeatures]))
 negativeFeatures = cachedweight.argsort()[0:5][::-1]
 print("The most negative features are: " + str(featureArray[negativeFeatures]) + \
-      " with weights of: " + str(cachedweight[negativeFeatures]))
+      " with weights of: " + str(finalWeight[negativeFeatures]))

Binary Feature Algorithms/avgSmartAgressiveMIRA.py
@@ -7,8 +7,7 @@ from Dev_Evaluator import DevEvaluator
 ## Averaged Smart Perceptron algorithm for binary classification
 ## of individuals earning less than or more than 50K/year.
 
-featureArray, trainDataArray = BinarizeData("train", sort=0, shuffle=1)
-devDataArray = BinarizeData("dev")
+trainDataArray, devDataArray, featureArray = BinarizeData(sort=0, shuffle=0)
 
 p = 1.0
 
@@ -49,51 +48,27 @@ while epochCount < totalEpoch:
             print("The error rate for epoch " + str(epochFraction) + \
                   " is " + str(devError) + "%")
 
-        if trainDataArray[i, -1] == '>50K':
+        if trainDataArray[i, -1] == 1:
             y = 1
 
         else:
             y = -1
 
-        idx = np.isin(featureArray, trainDataArray[i, 0:-1])
+        xi = trainDataArray[i, :-1]
 
-        decision = y*(weightVector[idx].sum() + weightVector[-1])
+        decision = y*(np.dot(weightVector, xi))
 
-        if decision < p:
+        if decision <= p:
 
-            if decision > 0:
-
-                marginCorrection = ( (p*y - np.sum(weightVector[idx]) - weightVector[-1]) / \
-                  np.sum(np.power(np.ones(len(trainDataArray[i, :])), 2)) )
+            marginCorrection = ( (y - np.dot(weightVector, xi)) / \
+              np.dot(xi, xi) )
             
-                weightVector[idx] = weightVector[idx] + \
-                marginCorrection*np.ones(len(trainDataArray[i, 0:-1]))
+            weightVector = weightVector + marginCorrection*xi
 
-                weightVector[-1] = weightVector[-1] + marginCorrection
+            weightVectorAveraged = weightVectorAveraged + \
+            currentTrainingCount * marginCorrection * xi
 
-                weightVectorAveraged[idx] = weightVectorAveraged[idx] + \
-                currentTrainingCount * marginCorrection * \
-                np.ones(len(trainDataArray[i, 0:-1]))
-
-                weightVectorAveraged[-1] = weightVectorAveraged[-1] + \
-                                           currentTrainingCount * marginCorrection
-
-            marginCorrection = ( (y - np.sum(weightVector[idx]) - weightVector[-1]) / \
-              np.sum(np.power(np.ones(len(trainDataArray[i, :])), 2)) )
-            
-            weightVector[idx] = weightVector[idx] + \
-            marginCorrection*np.ones(len(trainDataArray[i, 0:-1]))
-
-            weightVector[-1] = weightVector[-1] + marginCorrection
-
-            weightVectorAveraged[idx] = weightVectorAveraged[idx] + \
-            currentTrainingCount * marginCorrection * \
-            np.ones(len(trainDataArray[i, 0:-1]))
-
-            weightVectorAveraged[-1] = weightVectorAveraged[-1] + \
-                                       currentTrainingCount * marginCorrection
-
-##            check = y * (weightVector[idx].sum() + weightVector[-1])
+##            check = y * (np.dot(weightVector, xi))
 ##            print(check)
 
         currentTrainingCount += 1

Binary Feature Algorithms/avgSmartPerceptron.py
@@ -7,8 +7,7 @@ from Dev_Evaluator import DevEvaluator
 ## Averaged Smart Perceptron algorithm for binary classification
 ## of individuals earning less than or more than 50K/year.
 
-featureArray, trainDataArray = BinarizeData("train", sort=0, shuffle=0)
-devDataArray = BinarizeData("dev")
+trainDataArray, devDataArray, featureArray = BinarizeData(sort=0, shuffle=0)
 
 weightVector = np.zeros((len(featureArray)))
 weightVectorAveraged = np.zeros((len(featureArray)))
@@ -45,25 +44,20 @@ while epochCount < totalEpoch:
             print("The error rate for epoch " + str(epochFraction) + \
                   " is " + str(devError) + "%")
 
-        if trainDataArray[i, -1] == '>50K':
+        if trainDataArray[i, -1] == 1:
             y = 1
 
         else:
             y = -1
 
-        idx = np.isin(featureArray, trainDataArray[i, 0:-1])
+        xi = trainDataArray[i, :-1]
 
-        if y*(weightVector[idx].sum() + weightVector[-1]) <= 0:
+        if y*(np.dot(weightVector, xi)) <= 0:
             
-            weightVector[idx] = weightVector[idx] + \
-            y*np.ones(len(trainDataArray[i, 0:-1]))
+            weightVector = weightVector + y * xi
 
-            weightVector[-1] = weightVector[-1] + y
-
-            weightVectorAveraged[idx] = weightVectorAveraged[idx] + \
-            currentTrainingCount * y * np.ones(len(trainDataArray[i, 0:-1]))
-
-            weightVectorAveraged[-1] = weightVectorAveraged[-1] + currentTrainingCount * y
+            weightVectorAveraged = weightVectorAveraged + \
+            currentTrainingCount * y * xi
 
         currentTrainingCount += 1
 