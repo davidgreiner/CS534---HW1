184ad85d2b09abbd978c6822ae7836d09ca49fdd
Yogita Garud <garudyogitap@gmail.com>
2017-10-09 16:20:48 -0700

    Smart Perceptron

---
+++ Numerical Feature Algorithms/avgSmartPerceptron_Numerical.py
@@ -0,0 +1,79 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from replacingBinarized import BinarizeData
+from Dev_Evaluator import DevEvaluator
+
+## Basic Perceptron algorithm for binary classification
+## of individuals earning less than or more than 50K/year.
+
+trainDataArray, devDataArray, featureArray = BinarizeData(sort =0, shuffle=0)
+weightVector = np.zeros((len(trainDataArray[0, :-1])))
+weightVectorAveraged = np.zeros((len(trainDataArray[0, :-1])))
+epochCount = 0
+totalEpoch = 5
+numberTrainingData = len(trainDataArray)
+currentTrainingCount = 1
+bestErrorRate = 100.0
+epochIteration = 0
+
+devErrorPlot = []
+epochFractionPlot = []
+
+startTime = time.time()
+
+while epochCount < totalEpoch:
+
+    for i in range(0, numberTrainingData):
+
+        if currentTrainingCount % 1000 == 0:
+
+            devError = DevEvaluator(weightVector - (weightVectorAveraged / currentTrainingCount), devDataArray)
+
+            epochFraction = (i / numberTrainingData) + epochCount
+
+            devErrorPlot.append(devError)
+            epochFractionPlot.append(epochFraction)
+
+            if devError < bestErrorRate:
+                bestErrorRate = devError
+                epochIteration = epochFraction
+
+            print("The error rate for epoch " + str(epochFraction) + \
+                  " is " + str(devError) + "%")
+
+        if trainDataArray[i, -1] == 1:
+            y = 1
+
+        else:
+            y = -1
+
+
+        xi = trainDataArray[i, 0:-1]
+
+        if y*np.dot(xi, weightVector) <= 0:
+            
+            weightVector = weightVector + \
+            y*xi
+
+            weightVectorAveraged = weightVectorAveraged + y * currentTrainingCount * xi
+
+        currentTrainingCount += 1
+
+    epochCount += 1
+
+print("The program ran for %s seconds" % (time.time() - startTime))
+positiveFeatures = weightVector.argsort()[-5:][::-1]
+print("The most positive features are: " + str(featureArray[positiveFeatures]) + \
+      " with weights of: " + str(weightVector[positiveFeatures]))
+negativeFeatures = weightVector.argsort()[0:5][::-1]
+print("The most negative features are: " + str(featureArray[negativeFeatures]) + \
+      " with weights of: " + str(weightVector[negativeFeatures]))
+print("The best error rate was " + str(bestErrorRate) + " at epoch " + \
+      str(epochIteration))
+
+plt.plot(epochFractionPlot, devErrorPlot, 'ro')
+plt.axis([0, totalEpoch, 0, 100])
+plt.xlabel('Epoch Number')
+plt.ylabel('Error Rate, %')
+plt.show()
